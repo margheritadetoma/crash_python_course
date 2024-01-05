773ca550ac3d4d0e00123e96dcb2863a9c6f8438
Margherita De Toma <mdetoma@sissa.it>
2024-01-05 13:41:53 +0100

    all done, miss just some documentation

--- assignments/ex4/ex4_1_solution.py
+++ assignments/ex4/ex4_1_solution.py
@@ -1,40 +1,72 @@
 import math
+from assignments.ex3 import ex4_5_solution
+
 
 class Rational() :
 
-    """Documentation of the class
+    """
+    A class for converting a float into a rational number with
+    a given precision.
+
+
+    Attributes
+    ----------
+    num : float
+        the number to be handled
+    precision : float
+        the precision of the conversion into rational
+
+
+    Methods
+    -------
+    continued_fraction_approx(x, p)
+        Converts the float 'x' into a fraction up to the
+        precision 'p' using the continued fraction method
+
+    simplify_common_factors(x, y)
+        Simplifies the common factors of the
+        decomposition in prime factors
+
+    __str__():
+        Print as a string the number as rational
+
+    __repr__():
+        Print a string containing info about the float
+        input number and the requested precision
+
+    __add__():
+        Makes the addition between the rational representation
+        up to the specified precision of two float numbers
+
+    __mul__():
+        Makes the product between the rational representation
+        up to the specified precision of two float numbers
+
+    __eq__():
+        Tests if two numbers are equal checking their rational 
+        representation up to the specified precision
+
+    __hash__():
+        Uses the Cantor pairing function to make the a unique
+        number from the numerator and denominator of the rational
+        representation
+
     """
 
+
     def __init__(self, num, precision=1.e-5):
-        print(self.continued_fraction_approx(num, precision))
+        self.num, self.den = self.continued_fraction_approx(num, precision)
+        self.num, self.den = self.simplify_common_factors(self.num, self.den)
+        self.precision = precision
 
 
     def continued_fraction_approx(self, x, p): #x=num, p=precision
 
-        '''
-        1. Initialisation of variables:
-         * Let $x$ be the real number you want to approximate
-         * Set $a_0 = \floor{x} (the floor function gives the greatest integer less than or equal to $x$)
-         * Let $x_1 = x - a_0$
-        2. Generate continued fractions by iterating the following steps until the required precision (e.g. $p=10^{-5}$) is not met:
-         * Let $a_i$ be the floor of $1/x_i$
-         * update $x_{i+1} = \dfrac{1}{x_i} - a_i$
-         * check that $|\dfrac{n_i}{d_i}-x|>p$ if not, stop the iterations.
-           ($n_i$ and $d_i$ are the numerator and denominator of the approximate fraction at step $i$)
-        
-
-        > **Note that**
-        > * $n_0 = 1$ and $n_1 = a_0$
-        > * $d_0 = 0$ and $d_1 = 1$
-        > * $n_i = a_i \cdot n_{i-1} + n_{i-2}$
-        > * $d_i = a_i \cdot d_{i-1} + d_{i-2}$
-        '''
-
+        #Store the sign 
         sgn = +1
         if x < 0:
             x = -x
             sgn = -1
-
 
         ai = math.floor(x)
         xi = x - ai
@@ -42,46 +74,77 @@ class Rational() :
         dj, di = 0, 1
 
         while (abs(ni / di - x) > p):
-            print(ai, xi, ni, nj, di, dj)
             ai = math.floor(1 / xi)
             xi = 1 / xi - ai
 
             ni, nj = ai * ni + nj, ni
             di, dj = ai * di + dj, di
 
+        ni, di = self.simplify_common_factors(int(ni), int(di))
+
         return sgn*ni, di
 
 
+    def simplify_common_factors(self, x, y):
+        x_dec = ex4_5_solution.decomposition_prime_factors(int(x))
+        y_dec = ex4_5_solution.decomposition_prime_factors(int(y))
+        
+        for i in range(len(x_dec)):
+            for j in range(len(y_dec)):
+                if x_dec[i]==y_dec[j]:
+                    del y_dec[j]
+
+        num = 1
+        for i in range(0, len(x_dec)):
+            num = num *  x_dec[i]
+
+        den = 1
+        for i in range(0, len(y_dec)):
+            den = den *  y_dec[i]
+
+        return num, den
+
 
     def __str__(self):
-    	pass
-    def __repr__(self):
-    	pass
-    def __add__(self):
-    	pass
-    # [ ... ] overload all the arithmetic operators
-    def __eq__(self) :
-    	pass
-    # [ ... ] overload all the comparison operators
+        return f'{self.num}/{self.den}'
 
-    ####################################################
-    # Optional functions:
+
+    def __repr__(self):
+        return f'Rational({self.num/self.den}, precision={self.precision})'
+
+
+    def __add__(self, r2): #r2 = second rational
+        r_sum = Rational(1)
+        r_sum.num, r_sum.den = self.simplify_common_factors(self.num * r2.den + self.den * r2.num, self.den * r2.den)
+        return r_sum
+
+
+    def __mul__(self, r2):
+        r_prod = Rational(1) 
+        r_prod.num, r_prod.den = self.simplify_common_factors(self.num * r2.num, self.den * r2.den)
+        return r_prod
+
+
+    def __eq__(self, r2):
+        return (self.num == r2.num and self.den == r2.den)
+
     
-    def __hash__(self) :
-    	pass
-    def to_integer_low(self):
-    	"""Documentation of function `to_integer_low`"""
-    	pass
-    def to_integer_upp(self):
-    	"""Documentation of function `to_integer_upp`"""
-    	pass
+    def __hash__(self):
+        return (self.num + self.den) * (self.num + self.den + 1) // 2 + self.num 
+
 
 
 
 if __name__=='__main__':
-    r = Rational(3.1415926)
+    r1 = Rational(3.1415926)
+    r2 = Rational(3.14159)
+
+    #print(r1+r2)
+    #print(r1*r2)
+    #print(r1==r2)
 
 
-
-
+    #print({1:r1, 2:r2})
+    #print({r1:1, r2:2})
+    print(r1.decomposition_prime_factors())
 
